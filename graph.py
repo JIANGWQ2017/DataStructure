adc1aaf1ce98a53d5511f302a46cf127cc28482b
Jasper <jiangweiqi@xiaomi.com>
2019-07-19 22:44:05 -0400

    change

--- graph.py
+++ graph.py
@@ -14,9 +14,14 @@ class graph:
 			order = []
 			for i in range(self.nodeNum):
 				if visited[i] == 0:
-					order.extend(self._bfs(i,visited))
-			
+					
+					o = self._bfs(i,visited)
+					if o:
+						order.extend(o)
+					else:
+						return 
 			print(order)
+			
 		if algorithm.lower() == 'dfs':
 			order = []
 			for i in range(self.nodeNum):
@@ -24,7 +29,23 @@ class graph:
 					order.extend(self._dfs(i,visited))
 			
 			print(order)
-
+	
+	def checkLoop(self,algorithm):
+		if algorithm.lower() == 'kahn':
+			nodes = [1 for i in range(self.nodeNum)]
+			greeds = [sum(self.adjacentMatrix[i]) for i in range(self.nodeNum)]
+			print(self._kahn_uDAG(nodes,greeds))
+			
+			
+		if algorithm.lower() == 'bfs':
+			for i in range(self.nodeNum):
+				if visited[i] == 0:
+					
+					o = self._bfs(i,visited)
+					if not o:
+						return 
+			print('No Loop!')
+	
 	def _bfs(self,node,visited):
 		
 		# 遍历过的节点按顺序放在order list中
@@ -32,17 +53,21 @@ class graph:
 		# 入过栈的节点
 		inqueue = [0 for i in range(self.nodeNum)]
 		# 待访问的节点
-		stack = [node]
+		stack = [(node,node)]
 		inqueue[node] = 1
 		
 		while stack:
-			cur  = stack.pop(0)
+			cur,father  = stack.pop(0)
 			order.append(cur)
 			visited[cur] = 1
 			for i in range(len(self.adjacentMatrix[cur])):
-				if self.adjacentMatrix[cur][i] == 1 and inqueue[i] == 0:
-					stack.append(i)
-					inqueue[i]=1
+				if self.adjacentMatrix[cur][i] == 1:
+					if inqueue[i] == 0:
+						stack.append((i,cur))
+						inqueue[i]=1
+					elif i != father:
+						print('Loop existed in graph!')
+						return None
 		return order
 
 	
@@ -64,6 +89,25 @@ class graph:
 					stack.append(i)
 					inqueue[i] = 1
 		return order
+
+
+
+	def _kahn_uDAG(self, nodes, greeds):
+		count = 0  
+		for i in range(len(greeds)):
+			if greeds[i]<=1 and nodes[i] == 1:
+				count += 1
+				nodes[i] = 0
+				for j in range(len(self.adjacentMatrix[i])):
+					if self.adjacentMatrix[i][j] == 1:
+						greeds[j] -= 1
+		if count:
+			return self._kahn_DAG(nodes,greeds)
+		
+		return 1 in nodes
+	
+	 
+
 
 class edgeParser:
 	
@@ -104,8 +148,9 @@ def main():
 	adjacentMatrix = ep.generateAdjacentMatrix(nodes_num)
 	
 	G = graph(nodes_num, adjacentMatrix)
-	#G.traverse('bfs')
-	G.traverse('dfs')	
+	G.traverse('bfs')
+	#G.traverse('dfs')	
+	G.checkLoop('kahn')
 	
 if __name__ == "__main__":
 	main()
